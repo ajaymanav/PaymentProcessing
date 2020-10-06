5178b81dc78ce88d44b985ea16da5cedc45a08f5
ajaymanav <67000928+ajaymanav@users.noreply.github.com>
2020-10-06 21:02:27 +0530

    Add files via upload
    Added default Rule, Model and Enums

---
+++ PaymentProcessing/PaymentProcessing.BusinessRules/Enums/ProductType.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentProcessing.BusinessRules.Enums
+{
+    public enum ProductType
+    {
+         Physical,
+         Book,
+         Membership,
+         Video
+    }
+}
+
